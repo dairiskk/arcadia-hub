24840d7e3db29ebc1f5eff030ca5a3e5cbf7f44b
dairiskk <dairis.klesniks@gmail.com>
2024-12-18 23:45:49 +0200

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global Validation Pipe
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // Automatically strip properties that do not have decorators
     forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
@@ -12,6 +15,19 @@ async function bootstrap() {
       enableImplicitConversion: true, // Allow implicit type conversion
     },
   }));
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation for my NestJS application')
+    .setVersion('1.0')
+    .addBearerAuth() // Add Bearer token authentication, if required
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger UI served at /api
+
+  // Start Application
   await app.listen(3001);
 }
 bootstrap();
--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -3,6 +3,7 @@ import { MessageService } from './message.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { Message } from '@prisma/client';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
 @UseGuards(AuthGuard('jwt')) // Protect this route
 @Controller('messages')
@@ -10,8 +11,11 @@ export class MessageController {
   constructor(private readonly messageService: MessageService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Send message' })
+  @ApiResponse({ status: 201, description: 'The message has been successfully sent.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiBody({ schema: { example: { content: 'Hello!', receiverId: 2 } } })
   async sendMessage(@Body() createMessageDto: CreateMessageDto, @Request() req): Promise<Message> {
-
     createMessageDto.senderId = req.user.id;
     return this.messageService.sendMessage(createMessageDto);
   }
